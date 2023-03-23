2d27b310fdf8eb75145910fe841443e0cf08fef7
Vijay Padmakumar <p.vijay@litmus7.com>
2023-03-23 10:38:41 +0530

    updated port to 5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.send("OYE IT WOKRDS");
